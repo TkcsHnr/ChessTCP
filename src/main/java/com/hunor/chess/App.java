a5bdbd6d17d3f8f5053429b6a9a3bde830ce0793
Hunor <tkcshnr@gmail.com>
2020-04-26 09:41:51 +0200

    Initial commit

---
+++ src/main/java/com/hunor/chess/App.java
@@ -0,0 +1,23 @@
+package com.hunor.chess;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.hunor.chess {
+    requires javafx.controls;
+    exports com.hunor.chess;
+}
